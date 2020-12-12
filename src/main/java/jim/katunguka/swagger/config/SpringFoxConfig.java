6693feeeaa702b3768f6dd600f5fe67b6c463617
Jim Katunguka <jkatunguka@nexient.com>
2020-12-12 01:45:50 -0500

    more swagger configs

--- src/main/java/jim/katunguka/swagger/config/SpringFoxConfig.java
+++ src/main/java/jim/katunguka/swagger/config/SpringFoxConfig.java
@@ -1,12 +1,17 @@
 package jim.katunguka.swagger.config;
 
+import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -15,8 +20,8 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-//                .apis(RequestHandlerSelectors.any())
-//                .paths(PathSelectors.any())
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build();
     }
 }
